94daf25000cd35c4b35a72a9d8dd56c864962550
Min <barrs.mindy@gmail.com>
2019-03-22 17:11:16 -0400

    Removed the ImageCard.css file, and fixed the issue with the Image list and the grid.

--- src/compnents/ImageCard/ImageCard.js
+++ src/compnents/ImageCard/ImageCard.js
@@ -2,13 +2,32 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super();
+
+        this.state = { spanS: 0 };
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener("load", this.setSpans);
+    };
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans });
+    };
 
     render(){
         const { decsription, urls } = this.props.image;
 
         return(
-            <div>
-                <img alt={decsription} src={urls.regular} />
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img ref={this.imageRef} alt={decsription} src={urls.regular} />
             </div>
         );
     }
